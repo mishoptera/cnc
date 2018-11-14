35121adfc66506527b4c26ea7747fa1e6e68f76f
Misha Leong <mishaleong@gmail.com>
2018-11-14 13:40:48 -0800

    fixed back to all_wfreq. don't know why new name change wouldn't work but that;s that.

--- main.R
+++ main.R
@@ -17,27 +17,27 @@ library(ggpubr)
 library(stringr)
 
 # load files
-load('data/all.Rdata')
+load('data/all_wfreq.Rdata')
 load('data/cities.Rdata')
 
 # some last minute file cleaning
-all$scientific_name <- str_replace(all$scientific_name,"Columba livia domestica", "Columba livia")
-all$scientific_name <- as.factor(all$scientific_name)
+all_wfreq$scientific_name <- str_replace(all_wfreq$scientific_name,"Columba livia domestica", "Columba livia")
+all_wfreq$scientific_name <- as.factor(all_wfreq$scientific_name)
 
 # data subsets for later use
-plants <- all %>% filter(taxon_class_name %in% c("Magnoliopsida", "Liliopsida", "Polypodiopsida", "Pinopsida", "Agaricomycetes", "Lecanoromycetes"))
-animals <- all %>% filter(taxon_class_name %in% c("Arachnida", "Aves", "Gastropoda", "Insecta", "Amphibia", "Reptilia", "Mammalia"))
-dicots <- all %>% filter(taxon_class_name == "Magnoliopsida") %>% mutate (taxon="dicots")
-monocots <- all %>% filter(taxon_class_name == "Liliopsida") %>% mutate (taxon="monocots")
-ferns <- all %>% filter(taxon_class_name == "Polypodiopsida")%>% mutate (taxon="ferns")
-conifers <- all %>% filter(taxon_class_name == "Pinopsida") %>% mutate (taxon="conifers")
-birds <- all %>% filter(taxon_class_name == "Aves") %>% mutate (taxon="birds")
-insects <- all %>% filter(taxon_class_name == "Insecta") %>% mutate (taxon="insects")
-arachnids <- all %>% filter(taxon_class_name == "Arachnida") %>% mutate (taxon="arachnids")
-reptiles <- all %>% filter(taxon_class_name == "Reptilia") %>% mutate (taxon="reptiles")
-amphibians <- all %>% filter(taxon_class_name == "Amphibia") %>% mutate (taxon="amphibians")
-mammals <- all %>% filter(taxon_class_name == "Mammalia") %>% mutate (taxon="mammals")
-gastropods <- all %>% filter(taxon_class_name == "Gastropoda") %>% mutate (taxon="gastropods")
+plants <- all_wfreq %>% filter(taxon_class_name %in% c("Magnoliopsida", "Liliopsida", "Polypodiopsida", "Pinopsida", "Agaricomycetes", "Lecanoromycetes"))
+animals <- all_wfreq %>% filter(taxon_class_name %in% c("Arachnida", "Aves", "Gastropoda", "Insecta", "Amphibia", "Reptilia", "Mammalia"))
+dicots <- all_wfreq %>% filter(taxon_class_name == "Magnoliopsida") %>% mutate (taxon="dicots")
+monocots <- all_wfreq %>% filter(taxon_class_name == "Liliopsida") %>% mutate (taxon="monocots")
+ferns <- all_wfreq %>% filter(taxon_class_name == "Polypodiopsida")%>% mutate (taxon="ferns")
+conifers <- all_wfreq %>% filter(taxon_class_name == "Pinopsida") %>% mutate (taxon="conifers")
+birds <- all_wfreq %>% filter(taxon_class_name == "Aves") %>% mutate (taxon="birds")
+insects <- all_wfreq %>% filter(taxon_class_name == "Insecta") %>% mutate (taxon="insects")
+arachnids <- all_wfreq %>% filter(taxon_class_name == "Arachnida") %>% mutate (taxon="arachnids")
+reptiles <- all_wfreq %>% filter(taxon_class_name == "Reptilia") %>% mutate (taxon="reptiles")
+amphibians <- all_wfreq %>% filter(taxon_class_name == "Amphibia") %>% mutate (taxon="amphibians")
+mammals <- all_wfreq %>% filter(taxon_class_name == "Mammalia") %>% mutate (taxon="mammals")
+gastropods <- all_wfreq %>% filter(taxon_class_name == "Gastropoda") %>% mutate (taxon="gastropods")
 
 
 
@@ -68,15 +68,15 @@ ggsave("figures_n_tables/cnc_map.tiff", width = 20, height = 15, units = "cm")
 source('functions/cc_functions.r')
 
 # All Taxa
-cc_all <- cc_matrix(all)
+cc_all <- cc_matrix(all_wfreq)
 cc_all_env <- cc_env(cc_all)
 plot_cc_us(cc_all, cc_all_env, "All taxa")          # Figure 2
-plot_cc_region_4(all, "All taxa")             # Figure 3
+plot_cc_region_4(all_wfreq, "All taxa")             # Figure 4
 
 # All Plants
 cc_plants <- cc_matrix(plants) 
 cc_plants_env <- cc_env(cc_plants)
-plot_cc_us(cc_plants, cc_plants_env, "Plants")
+plot_cc_us(cc_plants, cc_plants_env, "Plants")      # Figure 3
 plot_cc_region_4(plants, "Plants")
 
 # All Animals
@@ -86,14 +86,14 @@ plot_cc_us(cc_animals, cc_animals_env, "Animals")
 plot_cc_region_4(animals, "Animals")
 
 # Create a table of PERMANOVA results for all taxa in all regions, nested by hometown.
-tab_all <- adonis.table.hometown(all) %>% mutate (taxon = "all")
+tab_all <- adonis.table.hometown(all_wfreq) %>% mutate (taxon = "all")
 tab_plants <- adonis.table.hometown(plants) %>% mutate (taxon = "plants")
 tab_animals <- adonis.table.hometown(animals) %>% mutate (taxon = "animals")
 tab <- bind_rows(tab_all, tab_plants, tab_animals)
 write.csv(tab, "figures_n_tables/permanova_results_hometown.csv")       # Table 2
 
 # Create a table of PERMANOVA results for all taxa in all regions, nested by land cover type.
-tab_all <- adonis.table.lc(all) %>% mutate (taxon = "all")
+tab_all <- adonis.table.lc(all_wfreq) %>% mutate (taxon = "all")
 tab_plants <- adonis.table.lc(plants) %>% mutate (taxon = "plants")
 tab_animals <- adonis.table.lc(animals) %>% mutate (taxon = "animals")
 tab <- bind_rows(tab_all, tab_plants, tab_animals)
@@ -132,7 +132,7 @@ lapply(taxa, function(i){
 })
 
 # to be able to add common names to table
-names <- all %>%
+names <- all_wfreq %>%
   select(scientific_name:common_name) %>%
   unique() 
 
