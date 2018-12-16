b2499c570dd4a8df5f7f424682ad0ff5fb203a81
Misha Leong <mishaleong@gmail.com>
2018-12-15 19:52:46 -0800

    cleaned up labels and condensed script for calculating the arm and cam slopes and making those plots.  Also cleaned up labels and condensed script for creating a by taxon table for species found in the majority of cities.  About to delete the script for making a histogram of the above.

--- urbanCore.R
+++ urbanCore.R
@@ -24,42 +24,31 @@ names <- all_inat %>%
   select(scientific_name:common_name) %>%
   unique()
 
-# filter list down to see which species were found in the majority of cities
-over8_obs <- all_inat %>%
-  group_by (scientific_name) %>%
-  mutate (num_cities = n_distinct(hometown)) %>%
-  filter(num_cities > 7) %>%
-  summarise(num_obs = n())%>%
-  arrange(desc(num_obs))
-over8_obs
-sum(over8_obs$num_obs)
-
-# creating a way to see the number of cities all species were found int
-numberCities <- all_inat %>%
+# creating a way to see the number of cities all species were found in
+over8 <- all_inat %>%
   group_by (taxon, scientific_name) %>%
-  summarise (num_cities = n_distinct(hometown)) %>%
+  summarise (num_cities = n_distinct(hometown), num_obs = n()) %>%
   left_join(names, by="scientific_name") %>%
   distinct(scientific_name, .keep_all = TRUE) %>%
-  select(taxon, common_name, scientific_name, num_cities) %>%
-  arrange(desc(num_cities))
-numberCities
-
-# combining tables to show species found in over 8 cities (how many cities & how many obs)
-over8 <- numberCities %>%
+  select(taxon, common_name, scientific_name, num_cities, num_obs) %>%
   filter(num_cities > 7) %>%
-  left_join(over8_obs, by = "scientific_name")
+  arrange(desc(num_cities))
 over8
 write.csv(over8, "figures_n_tables/over8.csv")
 
-# Comparisons between over8 with larger pool
+# Comparisons between over8 with larger pool grouped by taxon group
+# This is the larger pool
 all_stats <- all_inat %>%
   group_by (taxon) %>%
   summarise (total_species = n_distinct(scientific_name), total_obs = n())
 all_stats
 
+# this is to create blank variables for the taxon groups that do not have any species in >8 cities
 blanks <- tibble(taxon = c("gastropods", "ferns"), 
                  over8_species = c(0, 0), 
                  over8_obs = c(0, 0))
+
+# nice final data with the data all togther
 taxon_over8 <- over8 %>% 
   group_by(taxon) %>%
   summarise (over8_species = n_distinct(scientific_name), over8_obs = sum(num_obs)) %>%
@@ -69,68 +58,64 @@ taxon_over8 <- over8 %>%
   arrange(taxon)
 write.csv(taxon_over8, "figures_n_tables/taxon_over8.csv")
 
+
+###################################################
+# CALCULATING SLOPES BASED ON CAM AND ARM
+###################################################
+
 # a function to calculate the slope of the n:d4 points!
 get_slope <- function(n, d1, d2, d3, d4) { 
   x <- c(1:5)
   y <- c(n, d1, d2, d3, d4)
   slope_result <- lm(y~x, na.action=na.exclude)$coeff[[2]]
   return(slope_result)
-  }
-test <- big_over100obs %>%
+}
+
+# creating a table of the arm and cam slopes
+slopes <- big_over100obs %>%
   rowwise() %>%  #this is such an important thing!!!
   mutate(slope_cam = get_slope(n,d1, d2, d3, d4)) %>%
   mutate(slope_arm = get_slope(n.mean,d1.mean, d2.mean, d3.mean, d4.mean)) %>%
   ungroup() 
 
-test_labels_cam <- test %>%
+# plotting the cam slopes
+cam_labels_over <- slopes %>%
   filter(slope_cam >0) %>%
   filter(num_cities>7)
-
-test_labels_cam2 <- test %>%
+cam_labels_under <- slopes %>%
   filter(slope_cam < -1) %>%
   filter(num_cities>7)
-
-test_labels_arm <- test %>%
-  filter(slope_arm < -2) %>%
-  filter(num_cities>7)
-
-test_labels_arm2 <- test %>%
-  filter(slope_arm > 0 ) %>%
-  filter(num_cities>7)
-
-plot_cam <- ggplot(data=test,aes(x=num_cities,y=slope_cam, colour=taxon))+
+plot_cam <- ggplot(data=slopes,aes(x=num_cities,y=slope_cam, colour=taxon))+
   geom_point() + 
   labs(title = "evaluated with City Aggregation Metric", x = "number of cities", y = "slope of CAM") +
-  geom_text_repel(data = test_labels_cam, aes(x=num_cities, y=slope_cam, label = common_name)) + 
-  geom_text_repel(data = test_labels_cam2, aes(x=num_cities, y=slope_cam, label = common_name)) + 
+  geom_text_repel(data = cam_labels_over, aes(x=num_cities, y=slope_cam, label = common_name)) + 
+  geom_text_repel(data = cam_labels_under, aes(x=num_cities, y=slope_cam, label = common_name)) + 
   theme_bw() 
 plot_cam
 
-plot_arm <- ggplot(data=test,aes(x=num_cities,y=slope_arm, colour=taxon))+
+# plotting the arm slopes
+arm_labels_over <- slopes %>%
+  filter(slope_arm < -2) %>%
+  filter(num_cities>7)
+arm_labels_under <- slopes %>%
+  filter(slope_arm > 0 ) %>%
+  filter(num_cities>7)
+plot_arm <- ggplot(data=arm_slopes,aes(x=num_cities,y=slope_arm, colour=taxon))+
   geom_point() + 
-  geom_text_repel(data = test_labels_arm, aes(x=num_cities, y=slope_arm, label = common_name)) + 
-  geom_text_repel(data = test_labels_arm2, aes(x=num_cities, y=slope_arm, label = common_name)) + 
+  geom_text_repel(data = arm_labels_over, aes(x=num_cities, y=slope_arm, label = common_name)) + 
+  geom_text_repel(data = arm_labels_under, aes(x=num_cities, y=slope_arm, label = common_name)) + 
   labs(title = "evaluated with Averaged Ranking Metric", x = "number of cities", y = "slope of ARM") +
   theme_bw()+
   scale_y_reverse(lim=c(8, -8))
 plot_arm
 
-# Combine into one lovely figure and save
+# Combine the cam and arm slopes into one lovely figure and save
 plots <- ggarrange(plot_cam, plot_arm, labels = c("A", "B"), ncol = 1, nrow = 2)
 plots <- annotate_figure(plots,
                          top = text_grob("Biotic homogenization with urbanization intensity", face = "bold", size = 18))
 ggsave(plot = plots, filename = "figures_n_tables/bh_CAM_ARM.jpg", height = 24, width = 20, units = "cm")
 
-test_plot_armD <- ggplot(data=test %>%filter(taxon=="dicots"),aes(x=num_cities,y=slope_arm, colour=taxon))+
-  geom_point() + 
-  geom_text_repel(data = test_labels_arm%>%filter(taxon=="dicots"), aes(x=num_cities, y=slope_arm, label = common_name)) + 
-  theme_bw()+
-  scale_y_reverse()
-test_plot_armD
 
-  
-mean(test$slope_cam)
-mean(over8_wslopes$slope_cam)
 
 
 numberCities_plot <- ggplot(data=numberCities, aes(num_cities, fill = taxon, colour = taxon)) +
