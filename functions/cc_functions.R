dd2560fc11858df42b94e74fbe2d42b53666c208
Misha Leong <mishaleong@gmail.com>
2019-03-12 23:10:04 -0700

    added acript to caclulate AIC value

--- functions/cc_functions.R
+++ functions/cc_functions.R
@@ -251,8 +251,8 @@ adonis.table <- function(lc_subset) {
   p <- adonis_p(perm)
   
   adonis_table <- tribble(
-    ~Region, ~R2,  ~p,
-    "All USA", r2, usa_p
+    ~R2,  ~p,
+    r2, p
   )
   print(adonis_table)
 }
@@ -349,6 +349,12 @@ adonis_p <- function (perm) {
   p  <- perm[[1]][["Pr(>F)"]][[1]]
 }
 
+# ////////////////////
+# calculate AIC value
+adonis_aic <- function (perm) {
+  aic  <- AICc.PERMANOVA(perm)
+  return(aic$AIC)
+}
 
 # ////////////////////////////////////////
 # Plotting with species names on a regional scale. Still worth including?
@@ -403,5 +409,51 @@ plot_cc_region_species <- function (taxa, title) {
   
   filename <- paste("figures_n_tables/cc_region_", title, ".png", sep = "")
   ggsave(plot = all, filename = filename, height = 40, width = 40, units = "cm")
+  
+}
+
+# -----------------------
+## R Script from https://github.com/kdyson/R_Scripts/blob/master/AICc_PERMANOVA.R
+
+AICc.PERMANOVA <- function(adonis.model) {
+  
+  # check to see if object is an adonis model...
+  
+  if (!(adonis.model$aov.tab[1,1] >= 1))
+    stop("object not output of adonis {vegan} ")
+  
+  # Ok, now extract appropriate terms from the adonis model
+  # Calculating AICc using residual sum of squares (RSS) since I don't think that adonis returns something I can use as a liklihood function...
+  
+  RSS <- adonis.model$aov.tab[rownames(adonis.model$aov.tab) == "Residuals", "SumsOfSqs"]
+  MSE <- adonis.model$aov.tab[rownames(adonis.model$aov.tab) == "Residuals", "MeanSqs"]
+  
+  k <- ncol(adonis.model$model.matrix)# + 1 # add one for error variance
+  
+  nn <- nrow(adonis.model$model.matrix)
+  
+  # AIC : 2*k + n*ln(RSS)
+  # AICc: AIC + [2k(k+1)]/(n-k-1)
+  
+  # based on https://en.wikipedia.org/wiki/Akaike_information_criterion;
+  # https://www.researchgate.net/post/What_is_the_AIC_formula;
+  # http://avesbiodiv.mncn.csic.es/estadistica/ejemploaic.pdf
+  
+  # AIC.g is generalized version of AIC = 2k + n [Ln( 2(pi) RSS/n ) + 1]
+  # AIC.pi = k + n [Ln( 2(pi) RSS/(n-k) ) +1],
+  
+  AIC <- 2*k + nn*log(RSS)
+  AIC.g <- 2*k + nn * (1 + log( 2 * pi * RSS / nn))
+  AIC.MSE <- 2*k + nn * log(MSE)
+  AIC.pi <- k + nn*(1 + log( 2*pi*RSS/(nn-k) )   )
+  AICc <- AIC + (2*k*(k + 1))/(nn - k - 1)
+  AICc.MSE <- AIC.MSE + (2*k*(k + 1))/(nn - k - 1)
+  AICc.pi <- AIC.pi + (2*k*(k + 1))/(nn - k - 1)
+  
+  output <- list("AIC" = AIC, "AIC.g" = AIC.g, "AICc" = AICc,
+                 "AIC.MSE" = AIC.MSE, "AICc.MSE" = AICc.MSE,
+                 "AIC.pi" = AIC.pi, "AICc.pi" = AICc.pi, "k" = k)
+  
+  return(output)   
   
 }
